a9bd455eb8ec25f09bbab665ceed629ae7fae03b
Eyþór Máni Steinarsson <eythormani@gmail.com>
2016-01-27 10:01:30 +0000

    Major improvements

--- js.js
+++ js.js
@@ -23,5 +23,17 @@ for (var temp in pizzaArray) {
 	var tempDiv = document.createElement("div");
 	tempDiv.className="pizza";
 	tempDiv.innerHTML = "<div class=\"pizzaName\">" + pizzaArray[temp].name + "</div>";
+	toppingContainer = document.createElement("div");
+	toppingContainer.className = "toppingContainer";
+	for (var temptemp in pizzaArray[temp].toppings) {
+		var tempTopping = document.createElement("span");
+		tempTopping.className = "topping";
+		tempTopping.innerHTML = pizzaArray[temp].toppings[temptemp];
+		if (temptemp != pizzaArray[temp].toppings.length -1) {
+			tempTopping.innerHTML +=" & ";
+		};
+		toppingContainer.appendChild(tempTopping);
+	};
+	tempDiv.appendChild(toppingContainer);
 	mainThing.appendChild(tempDiv);
 };
